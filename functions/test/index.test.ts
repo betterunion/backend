9c6feb38bf442855f9d7ae76f10ef8a87885daf4
Aidan Kelley <aidanmkelley@gmail.com>
2019-01-07 13:39:43 -0600

    started to write unit tests for the functions

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -1,3 +1,31 @@
-describe("test", () => {
-    it("is pending");
+import * as firebaseFunctionsTest from "firebase-functions-test";
+import * as functions from "firebase-functions";
+import "mocha";
+import {expect} from "chai";
+
+let onlineConfig = {
+    databaseURL: "https://betterunion-8ac8f.firebaseio.com",
+    projectId: "betterunion-8ac8f"
+};
+
+let pathToKey = "../keys/betterunion-8ac8f-c38aaf15539b.json";
+
+const test = firebaseFunctionsTest(onlineConfig, pathToKey);
+
+import * as myFunctions from "../src/index";
+
+describe("handleNewUser",  () => {
+    const wrapped = test.wrap(myFunctions.handleNewUser);
+
+    it("handles basic user", () => {
+        const basicUser: functions.auth.UserRecord = {
+            uid: "basicUser",
+            email: "basicUser@fake.email",
+            emailVerified: false,
+            displayName: "Basic User"
+        }
+    });
 });
+
+
+
